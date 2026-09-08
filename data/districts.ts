47e8a46fdd3d3b6a76cf77a5eda08d49cb1a7bcd
yastar123 <eduj2164@gmail.com>
2026-09-08 15:30:59 +0700

    Initial commit

---
+++ data/districts.ts
@@ -0,0 +1,18 @@
+export type District = { name: string; city: string }
+
+const districtsByCity: Record<string, string[]> = {
+  bandung: ['Andir', 'Antapani', 'Arcamanik', 'Astanaanyar', 'Babakan Ciparay', 'Bandung Kidul', 'Bandung Kulon', 'Bandung Wetan', 'Batununggal', 'Bojongloa Kaler', 'Bojongloa Kidul', 'Buahbatu', 'Cibeunying Kaler', 'Cibeunying Kidul', 'Cibiru', 'Cicendo', 'Cidadap', 'Cinambo', 'Coblong', 'Gedebage', 'Kiaracondong', 'Lengkong', 'Mandalajati', 'Panyileukan', 'Rancasari', 'Regol', 'Sukajadi', 'Sukasari', 'Sumur Bandung', 'Ujungberung'],
+  jakarta: ['Cengkareng', 'Grogol Petamburan', 'Kebon Jeruk', 'Kembangan', 'Palmerah', 'Taman Sari', 'Tambora', 'Tanah Abang', 'Menteng', 'Senen', 'Gambir', 'Kemayoran', 'Sawah Besar', 'Cempaka Putih', 'Tebet', 'Setiabudi', 'Mampang Prapatan', 'Pancoran', 'Pasar Minggu', 'Kebayoran Baru', 'Kebayoran Lama', 'Cilandak', 'Jagakarsa', 'Pulogadung', 'Jatinegara', 'Duren Sawit', 'Kramat Jati', 'Pasar Rebo', 'Cakung', 'Matraman', 'Kelapa Gading', 'Koja', 'Pademangan', 'Penjaringan', 'Tanjung Priok', 'Cilincing'],
+  medan: ['Medan Amplas', 'Medan Area', 'Medan Barat', 'Medan Baru', 'Medan Belawan', 'Medan Deli', 'Medan Denai', 'Medan Helvetia', 'Medan Johor', 'Medan Kota', 'Medan Labuhan', 'Medan Maimun', 'Medan Marelan', 'Medan Perjuangan', 'Medan Petisah', 'Medan Polonia', 'Medan Selayang', 'Medan Sunggal', 'Medan Tembung', 'Medan Timur', 'Medan Tuntungan'],
+}
+
+export function districtsForCity(citySlug: string, cityName: string): District[] {
+  const names = districtsByCity[citySlug] ?? [`Pusat ${cityName}`, `Kawasan ${cityName}`]
+  return names.map((name) => ({ name: `Kecamatan ${name}`, city: cityName }))
+}
+
+export function districtMapsUrl(district: District) {
+  return `https://www.google.com/maps/search/?api=1&query=${encodeURIComponent(`${district.name}, ${district.city}`)}`
+}
+
+export { districtsByCity }
